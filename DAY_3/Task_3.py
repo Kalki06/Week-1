35092aad5afdfc118a79249026f1e935266e2cca
Manjeet Singh Tomar <tomar32590@gmail.com>
2026-02-14 21:30:16 +0530

    Day 3: for loops and repetition

---
+++ DAY_3/Task_3.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number : "))
+
+sum = 0
+
+for i in range(num + 1):
+    sum += i
+
+print (sum)
---
+++ DAY_3/Task_4.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number : "))
+i = 1
+for i in range(11):
+    print(f"{num} x {i} = {num*i}")
